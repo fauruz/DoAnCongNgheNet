512cb4410dcc0cdfa7438277bb99c15185f696a2
fauruz <le.phong1456@gmail.com>
2023-12-18 14:16:16 +0700

    doan1.8 chuc nang form nha xuat ban

--- quanlythuvien/PublisherForm.Designer.cs
+++ quanlythuvien/PublisherForm.Designer.cs
@@ -48,7 +48,6 @@
             // 
             // btnReview
             // 
-            this.btnReview.Enabled = false;
             this.btnReview.Location = new System.Drawing.Point(579, 238);
             this.btnReview.Margin = new System.Windows.Forms.Padding(4);
             this.btnReview.Name = "btnReview";
@@ -56,6 +55,7 @@
             this.btnReview.TabIndex = 41;
             this.btnReview.Text = "Xem";
             this.btnReview.UseVisualStyleBackColor = true;
+            this.btnReview.Click += new System.EventHandler(this.btnReview_Click);
             // 
             // btnExit
             // 
@@ -66,6 +66,7 @@
             this.btnExit.TabIndex = 40;
             this.btnExit.Text = "Thoát";
             this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
             // btnDelete
             // 
@@ -76,6 +77,7 @@
             this.btnDelete.TabIndex = 39;
             this.btnDelete.Text = "Xóa";
             this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
             // 
             // btnUpdate
             // 
@@ -86,6 +88,7 @@
             this.btnUpdate.TabIndex = 38;
             this.btnUpdate.Text = "Sửa";
             this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
             // 
             // btnAdd
             // 
@@ -96,6 +99,7 @@
             this.btnAdd.TabIndex = 37;
             this.btnAdd.Text = "Thêm";
             this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // label5
             // 
@@ -176,6 +180,7 @@
             this.dgvPublisher.RowHeadersWidth = 51;
             this.dgvPublisher.Size = new System.Drawing.Size(753, 234);
             this.dgvPublisher.TabIndex = 52;
+            this.dgvPublisher.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvPublisher_CellContentClick);
             // 
             // label1
             // 
--- quanlythuvien/PublisherForm.cs
+++ quanlythuvien/PublisherForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using quanlythuvien.Objects;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,96 @@ namespace quanlythuvien
 {
     public partial class PublisherForm : Form
     {
+        dbThuVienDataContext db = new dbThuVienDataContext();
         public PublisherForm()
         {
             InitializeComponent();
+        }
+        private void clearForm()
+        {
+            txtPublisherId.Text = string.Empty;
+            txtPublisherName.Text = string.Empty;
+        }
+        private bool checkValid()
+        {
+            if (txtPublisherId.Text == string.Empty)
+            {
+                MessageBox.Show("Nhập mã nhà xuất bản");
+                return false;
+            }
+            if (txtPublisherName.Text == string.Empty)
+            {
+                MessageBox.Show("Nhập tên nhà xuất bản");
+                return false;
+            }
+            return true;
+        }
+        private NHAXUATBAN storeNhaXuatBan()
+        {
+            NHAXUATBAN nxb = new NHAXUATBAN();
+            nxb.MANXB = txtPublisherId.Text;
+            nxb.TEN = txtPublisherName.Text;
+            return nxb;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (checkValid())
+            {
+                NHAXUATBAN nxb = storeNhaXuatBan();
+                db.NHAXUATBANs.InsertOnSubmit(nxb);
+                db.SubmitChanges();
+                MessageBox.Show("Thêm thành công!");
+                btnReview_Click(sender, e);
+                clearForm();
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (checkValid())
+            {
+                NHAXUATBAN nxb = db.NHAXUATBANs.Single(s => s.MANXB == txtPublisherId.Text);
+                nxb.TEN = txtPublisherName.Text;
+                db.SubmitChanges();
+                MessageBox.Show("Sửa thành công!");
+                btnReview_Click(sender, e);
+                clearForm();
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (checkValid())
+            {
+                NHAXUATBAN nxb = db.NHAXUATBANs.Single(s => s.MANXB == txtPublisherId.Text);
+                db.NHAXUATBANs.DeleteOnSubmit(nxb);
+                db.SubmitChanges();
+                MessageBox.Show("Xóa thành công!");
+                btnReview_Click(sender, e);
+                clearForm();
+            }
+        }
+
+        private void btnReview_Click(object sender, EventArgs e)
+        {
+            List<NHAXUATBAN> lnxb = db.NHAXUATBANs.ToList();
+            dgvPublisher.DataSource = lnxb;
+            dgvPublisher.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgvPublisher.Columns[0].HeaderText = "Mã nhà xuất bản";
+            dgvPublisher.Columns[1].HeaderText = "Tên nhà xuất bản";
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void dgvPublisher_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            NHAXUATBAN nxb = db.NHAXUATBANs.First(s => s.MANXB == dgvPublisher.Rows[e.RowIndex].Cells[0].Value);
+            txtPublisherId.Text = nxb.MANXB;
+            txtPublisherName.Text = nxb.TEN;
         }
     }
 }
